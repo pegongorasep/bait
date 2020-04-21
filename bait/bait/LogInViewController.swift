c35034817324968e281a8966e220218161dff07e
Pedro Antonio Góngora <pedro.gongora@dacodes.com.mx>
2020-04-21 09:59:24 -0500

    Added Login View Controller

---
+++ bait/bait/LogInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LogInViewController.swift
+//  bait
+//
+//  Created by Pedro Antonio Góngora Sepúlveda on 21/04/20.
+//  Copyright © 2020 Pedro Antonio Góngora Sepúlveda. All rights reserved.
+//
+
+import Foundation
